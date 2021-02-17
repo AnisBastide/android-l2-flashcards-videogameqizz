2d36e10f177b81db8e66aa4f8a605d1332e1a9f1
Full Name <anis.bastide@gmail.com>
2021-02-17 11:23:25 +0100

    flashcard template

---
+++ app/src/main/java/com/theo/videogameqizz/FlashCard.java
@@ -0,0 +1,14 @@
+package com.theo.videogameqizz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FlashCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.flash_card);
+    }
+}
