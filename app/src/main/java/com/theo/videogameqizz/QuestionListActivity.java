128816f28b8a6e426095ffa71eb34cdcc2370cae
Theovady Moutty <theomoutty@gmail.com>
2021-02-19 02:14:02 +0100

    origin theo add question question list

--- app/src/main/java/com/theo/videogameqizz/QuestionListActivity.java
+++ app/src/main/java/com/theo/videogameqizz/QuestionListActivity.java
@@ -23,15 +23,33 @@ public class QuestionListActivity extends AppCompatActivity{
         questions = new ArrayList<>();
             questions.add(new Question("Quel est ce jeu ?",R.drawable.supersmashbros,
                     new Answer("super smash bros","mario","call of duty")));
-            questions.add(new Question("Quel est ce jeu ?",R.drawable.supersmashbros,
-                    new Answer("super smash bros","mario","call of duty")));
-            questions.add(new Question("Quel est ce jeu ?",R.drawable.supersmashbros,
-                    new Answer("super smash bros","mario","call of duty")));
-            questions.add(new Question("Quel est ce jeu ?",R.drawable.supersmashbros,
-                    new Answer("super smash bros","mario","call of duty")));
-            questions.add(new Question("Quel est ce jeu ?",R.drawable.supersmashbros,
-                    new Answer("super smash bros","mario","call of duty")));
+
+            questions.add(new Question("Quel est ce jeu ?",R.drawable.fortnite,
+                    new Answer("Fortinte","PUBG","H1Z1")));
+
+            questions.add(new Question("Quel est ce jeu ?",R.drawable.legendofzeldabreathofthewild,
+                    new Answer("Zelda Breath of the wild","Zelda Ocarina of time","Zelda Oracle of season")));
+
+            questions.add(new Question("Quel est ce jeu ?",R.drawable.persona,
+                    new Answer("Persona 5","Divinity Original Sin","Mario et Luigi")));
+
+            questions.add(new Question("Comment s’appelle ce protagoniste ?",R.drawable.geralt,
+                    new Answer("Geralt de Riv","Gutz","Garen")));
+
+            questions.add(new Question("Comment s’appelle cette map de Call of duty ?",R.drawable.blackopmap,
+                    new Answer("yacht","nuketown","Terminal")));
+
+            questions.add(new Question("Quel est ce jeux video ?",R.drawable.finalfantasyvi,
+                    new Answer("Final Fantasy 6","Final Fantasy 5","Dragon Quest 6")));
+
+            questions.add(new Question("Qui est ce Boss ?",R.drawable.genshinimpact,
+                    new Answer("boreas","Sif","Rock")));
+
+            questions.add(new Question("Qui est le premier seigneur des cendres de dark soul 1 ?",R.drawable.darksoulsgwyn,
+                    new Answer("Gwen","Gwendir","Gundir")));
+
         adapter = new QuestionsList(questions);
+
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
